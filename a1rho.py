6aff583f7e706a3ca27b4b1c759d8b65f30c18ce
Andrei Rabusov <a.rabusov@tum.de>
2024-02-08 11:51:14 +0100

    Make pics presentation ready

--- a1rho.py
+++ a1rho.py
@@ -36,27 +36,32 @@ ampl = np.array ([ Fa1a1p (m3pi**2, 1.275)*
     BW (np.arange (2*mpi, m3pi, dM2pi)**2, 0.770, 0.150, 1)
     for m3pi in m3pis])
 
-def magnitude_1D ():
+def plot_iop(phase=False):
     qqs = m3pis**2
-    fig, ax = plt.subplots(1, 2, figsize=(18,8))
     FF=np.vectorize (bw)
-    a1widths = [0.150, 0.3, 0.4, 0.6]
+    a1widths = [0.250, 0.3, 0.4, 0.6]
     for a1width in a1widths:
-        sample = FF (qqs, 1.260, a1width, 1)
-        sample /= np.nanmax (np.abs (sample))
-        ax[0].plot (m3pis, np.abs (sample), lw=2,
-                label='$\\Gamma(\\text{a}_1)='+str(int(1000*a1width))+'$ [GeV]')
-        ax[1].plot (m3pis, np.angle (sample, True), lw=2)
-    ax[0].grid(True)
-    ax[0].legend ()
-    ax[0].set (xlabel='$m(3\pi)$ [GeV]', ylabel='$\\text{a}_1(1260)$ magnitude/max magnitude')
-    ax[0].set (title='$m[\\text{a}_1(1260)]=1.26$ [GeV]')
-    ax[1].set (xlabel='$m(3\pi)$ [GeV]', ylabel='$\\text{a}_1(1260)$ phase [deg]')
-    ax[1].grid(True)
-
-if __name__=="__main__":
-    conf_mlp (font_size=14)
-    magnitude_1D ()
+        sample = FF (qqs, 1.270, a1width, 1)
+        sample /= np.abs (sample[0])
+        if not phase:
+            plt.plot (m3pis, np.abs (sample)**2, lw=2,
+                        label='$\\Gamma='+str(int(1000*a1width))+'$ [MeV]')
+        else:
+            plt.plot(m3pis, np.angle (sample, True), lw=2,
+                     label='$\\Gamma='+str(int(1000*a1width))+'$ [MeV]')
+    plt.grid(True)
+    plt.legend ()
+    plt.xlabel('$m_{3\pi}$ [GeV]')
+    if not phase:
+        plt.ylabel('$|\\mathcal{A}|^2$')
+    else:
+        plt.ylabel('Phase [$^\circ$]')
+    plt.grid(True)
     plt.tight_layout ()
     plt.show ()
     plt.close ()
+
+if __name__=="__main__":
+    conf_mlp (font_size=18)
+    plot_iop(False)
+    plot_iop(True)
